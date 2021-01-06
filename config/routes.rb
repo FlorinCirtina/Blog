73dd98b297b1f41c3dae2fa8904d96744c8b0357
FlorinCirtina <florin.cirtina@gmail.com>
2021-01-06 13:18:23 +0200

    full app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do	
   root 'pages#home'
+  get 'about', to: 'pages#about'
+  resources :articles#, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  resources :categories, except: [:destroy]
 end
